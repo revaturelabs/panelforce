09482dc9c3f21c3b6616d5685773cc89a6a520f6
Cody <cody.bennett.t@gmail.com>
2019-06-19 16:28:28 -0400

    Cody's changes

--- force-app/main/default/aura/PanelViewMain/PanelViewMainController.js
+++ force-app/main/default/aura/PanelViewMain/PanelViewMainController.js
@@ -1,5 +1,90 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit: function(component, event, helper) {
+        var contactID = component.get("v.recordId");
+        var action = component.get("c.getTrack");
+        action.setParams({"contactID" : contactID});
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.track", response.getReturnValue());
+            }
+            else if (state === "INCOMPLETE") {
+                console.log("Incomplete.");
+            }
+            else if (state === "ERROR") {
+                console.log(response.getError()[0].message);
+            }
+        });
+        $A.enqueueAction(action);
 
+        var action2 = component.get("c.getNumberofTries");
+        action2.setParams({"contactID" : contactID});
+        action2.setCallback(this, function(response2) {
+            console.log(response2.getReturnValue());
+            var state = response2.getState();
+            if (state === "SUCCESS") {
+                component.set("v.numberofTries", response2.getReturnValue());
+            }
+            else if (state === "INCOMPLETE") {
+                console.log("Incomplete.");
+            }
+            else if (state === "ERROR") {
+                console.log(response2.getError()[0].message);
+            }
+        });
+        $A.enqueueAction(action2);
+      },
+      
+      handlePanelTrackEvent : function(cmp, event) {
+        var panelTrack = event.getParam("updateTrack");
+        cmp.set("v.trackFromEvent", panelTrack);
+        var tEvent = cmp.get("v.trackFromEvent");
+        console.log(tEvent);
+    },    
+    
+    handlePanelCategoriesEvent : function(cmp, event) {
+        var listOfCategories = event.getParam("updateCategories");
+        cmp.set("v.listFromEvent",listOfCategories);
+        var lEvent = cmp.get("v.listFromEvent");
+        console.log(lEvent[0]);
+  },    
+    
+    handleInterviewAppStateEvent : function(cmp, event) {
+        var interviewAppState = event.getParam("state");
+        console.log(JSON.stringify(interviewAppState));
+        // cmp.set("v.listFromEvent", JSON.stringify(interviewAppState));
+    },
+
+    save : function(component, event, helper) {
+        var lEventCategories = component.get("v.listFromEvent");
+        console.log(lEventCategories);
+
+        // var lEventTrack = component.get("v.trackFromEvent");
+        // console.log(lEventTrack);
+
+        var sCategories = component.get("c.saveAssessment");
+        sCategories.setParams({"pcList" : lEventCategories});//,
+                                // "track" : lEventTrack});
+        sCategories.setCallback(this,function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("The save was successful.");
+            }
+            else if (state === "INCOMPLETE") {
+                console.log("Incomplete");
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });      
+        $A.enqueueAction(sCategories); 
     }
 })

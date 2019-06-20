28c395b95d6d3fc627793233ad26e02e81f6a667
yfedorovskyi@gmail.com <yfedorovskyi@gmail.com>
2019-06-20 10:09:31 -0400

    Parth merge

--- force-app/main/default/aura/InterviewMainScore/InterviewMainScoreController.js
+++ force-app/main/default/aura/InterviewMainScore/InterviewMainScoreController.js
@@ -1,5 +1,53 @@
+//handler click for adding +1
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(cmp, event, helper) {
+        let allCats = cmp.get("v.categories");
+        let indx = cmp.get("v.currentIndex");
+        cmp.set("v.Max_Score", allCats[indx].maxScore__c);
+    },
 
+    handleClickA1: function (cmp, event, helper) {
+        var selectedButtonLabel = event.getSource().get("v.label");
+        console.log("Button label: " + selectedButtonLabel);
+       helper.helperMethod(cmp, 1);
+    },
+
+//handler click for adding +3
+
+    handleClickA3: function (cmp, event, helper) {
+        var selectedButtonLabel = event.getSource().get("v.label");
+        console.log("Button label: " + selectedButtonLabel);
+        helper.helperMethod(cmp, 3);
+    },
+
+//handler click for adding +5
+    handleClickA5: function (cmp, event, helper) {
+        var selectedButtonLabel = event.getSource().get("v.label");
+        console.log("Button label: " + selectedButtonLabel);
+        helper.helperMethod(cmp, 5);
+    },
+
+//handler click for subtracting -1
+    handleClickS1: function (cmp, event, helper) {
+        var selectedButtonLabel = event.getSource().get("v.label");
+        console.log("Button label: " + selectedButtonLabel);
+        helper.helperMethod(cmp, -1);
+    },
+
+
+//handler click for subtracting -3
+    handleClickS3: function (cmp, event, helper) {
+        var selectedButtonLabel = event.getSource().get("v.label");
+        console.log("Button label: " + selectedButtonLabel);
+        helper.helperMethod(cmp, -3);
+    },
+
+
+//handler click for subtracting -5
+    handleClickS5: function (cmp, event, helper) {
+        var selectedButtonLabel = event.getSource().get("v.label");
+        console.log("Button label: " + selectedButtonLabel);
+        helper.helperMethod(cmp, -5);
     }
+
 })
--- force-app/main/default/aura/InterviewMainScore/InterviewMainScoreHelper.js
+++ force-app/main/default/aura/InterviewMainScore/InterviewMainScoreHelper.js
@@ -1,5 +1,33 @@
 ({
-    helperMethod : function() {
+    helperMethod : function(component,value) {
+        //logic here
 
+        //assigns the current score to the component
+        var curr_score = component.get("v.Current_Score");
+
+        var maxScore = component.get("v.Max_Score");
+
+        //adds the value to the current score
+        curr_score += value;
+
+        if(curr_score > maxScore)
+        {
+            curr_score = maxScore;
+            alert("Current Score cannot be greater than the Max Score.");
+        }
+
+        else if(curr_score<0)
+        {
+            curr_score = 0;
+            alert("Current Score cannot be Negative.");
+        }
+
+        //retrieves the value from the component
+        var action = component.set("v.Current_Score",curr_score);
+
+        var action = component.set("v.category.Score__c",curr_score);
+        
+        
     }
+    
 })

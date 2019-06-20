a9e4a4f86dabcf6dea26078933c0349af1c0ed37
yfedorovskyi@gmail.com <yfedorovskyi@gmail.com>
2019-06-20 09:53:29 -0400

    Josh merge

--- force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesController.js
+++ force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesController.js
@@ -1,5 +1,107 @@
+// {!REQUIRESCRIPT('/soap/ajax/37.0/connection.js')}
 ({
-    myAction : function(component, event, helper) {
 
+    loadList: function(component, event, helper) {
+        console.log("loadList Fired!!");
+        component.set("v.searchText", "");
+        let assessmentID = component.get("v.recordId");
+        // console.log(assessmentID);
+        // result = sforce.connection.query("Select Name, Id FROM AssessmentLineItem__c WHERE PH_Assessment__c = " + assessmentID);
+        // records = result.getArray("AssessmentLineItem__c");
+        var action = component.get("c.fetchLineItems");
+        var items;
+        action.setParams({ PanelId: assessmentID });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("Success!");
+                items = response.getReturnValue();
+                component.set("v.records", items);
+                items.forEach(listItem => {
+                    console.log("item: " + JSON.stringify(listItem));
+                    helper.addListItem(component, listItem);
+                });
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    filterResults: function(component, event, helper) {
+        let buttonSelection = component.get("v.buttonSelection");
+        let searchText = component.get("v.searchText");
+        let records = component.get("v.records");
+        component.set("v.body", "");
+        records.forEach(record => {
+            // Only add records with name containing searchText
+            if (!record["Name"].toUpperCase().includes(searchText.toUpperCase())) {
+                return;
+            }
+            // console.log("selection: " + buttonSelection);
+            // console.log("status: " + record["Status__c"]);
+            if (buttonSelection == "Pass" && record["Status__c"] == false) {
+                return;
+            }
+            if (buttonSelection == "Fail" && record["Status__c"] == true) {
+                return;
+            }
+            helper.addListItem(component, record);
+            // console.log("adding item");
+            // console.log("Name: " + record["Name"]);
+        });
+        // console.log(searchText);
+    },
+
+    buttonClick: function(component, event, helper) {
+        let source = event.getSource().getLocalId();
+        console.log("Source: " + source);
+        let buttonSelection = "";
+        let buttonGroup = component.find("filterButtons").get("v.body");
+        buttonGroup.forEach(button => {
+            button.set("v.variant", "neutral");
+        });
+        switch (source) {
+            case "allButton":
+                buttonSelection = "All";
+                component.find("allButton").set("v.variant", "brand");
+                break;
+            case "passButton":
+                buttonSelection = "Pass";
+                component.find("passButton").set("v.variant", "brand");
+                break;
+            case "failButton":
+                buttonSelection = "Fail";
+                component.find("failButton").set("v.variant", "brand");
+                break;
+            default:
+                console.log("error");
+                break;
+        }
+        component.set("v.buttonSelection", buttonSelection);
+    },
+
+    labelClick: function(component, event, helper) {
+        console.log("label clicked!");
+        event.getSource().set("v.readonly", false);
+        // console.log(source);
+    },
+    labelBlur: function(component, event, helper) {
+        console.log("label clicked!");
+        event.getSource().set("v.readonly", true);
+        // console.log(source);
+    },
+
+    updateCategories: function(component, event, helper) {
+        let customEvent = component.getEvent("updateCategoriesEvent");
+        // let records = [{...},{...}];
+        let records = [{
+            sobjectType: "Contact",
+            Param1: "I_am_a_param",
+            Param2: "I_am_another_param"
+        }];
+        customEvent.setParams({
+            // Some placeholder stuff here
+            updateCategories: records
+        });
+        customEvent.fire();
     }
-})
+});
--- force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesHelper.js
+++ force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesHelper.js
@@ -1,5 +1,64 @@
 ({
-    helperMethod : function() {
+    helperMethod: function() {
 
+    },
+    addListItem: function(component, listItem) {
+        $A.createComponents(
+            [
+                ["aura:html", { tag: "li", HTMLAttributes: { class: "slds-item CategoryItem" } }],
+                ["lightning:layout", { class: "CategoryLayout" }],
+                // Category name
+                [
+                    "lightning:layoutItem",
+                    { class: "CategoryName", padding: "around-small" }
+                ],
+                ["lightning:input", { type: "text", name: "CategoryName", value: listItem["Name"], readonly: "true" }],
+                // Category Score
+                [
+                    "lightning:layoutItem",
+                    { class: "CategoryScore", padding: "around-small" }
+                ],
+                ["lightning:input", { type: "text", name: "Score", value: listItem["Score__c"], readonly: "true", onblur: component.getReference("c.labelBlur"), onclick: component.getReference("c.labelClick") }],
+                // Category pass/fail flag
+                [
+                    "lightning:layoutItem",
+                    { class: "CategoryResult", padding: "around-small" }
+                ],
+                ["lightning:icon", { iconName: (listItem["Status__c"]) ? "action:approval" : "action:close", size: "xx-small" }]
+            ],
+            function(subparts, status, errorMessage) {
+                //Add the new button to the body array
+                if (status === "SUCCESS") {
+                    // Get component's v.body to populated list
+                    var body = component.get("v.body");
+                    // li and layout container
+                    var liContainer = subparts[0];
+                    var layout = subparts[1];
+                    // Category Name
+                    var categoryNameItem = subparts[2];
+                    var categoryName = subparts[3];
+                    categoryNameItem.set("v.body", categoryName);
+                    // Category Score
+                    var scoreItem = subparts[4];
+                    var score = subparts[5];
+                    scoreItem.set("v.body", score);
+                    // Category flag icon
+                    var iconItem = subparts[6];
+                    var icon = subparts[7];
+                    iconItem.set("v.body", icon);
+                    // Set, push, and commit
+                    layout.set("v.body", [categoryNameItem, scoreItem, iconItem]);
+                    liContainer.set("v.body", layout);
+                    body.push(liContainer);
+                    component.set("v.body", body);
+                } else if (status === "INCOMPLETE") {
+                    console.log("No response from server or client is offline.");
+                    // Show offline error
+                } else if (status === "ERROR") {
+                    console.log("Error: " + errorMessage);
+                    // Show error message
+                }
+            }
+        );
     }
 })

48602526434005388df3357c1532e3b4b8f6044e
James_Cole <james@dfcint.com>
2019-06-21 11:40:29 -0400

    Set up Finish button

--- force-app/main/default/aura/InterviewMain/InterviewMainController.js
+++ force-app/main/default/aura/InterviewMain/InterviewMainController.js
@@ -9,7 +9,6 @@
 
         var action1 = component.get("c.getTrainAssign");
         var recordId = component.get("v.recordId");
-        console.log("recordId: " + recordId);
         action1.setParams({ recId : recordId });
         action1.setCallback(this, function(response) {
             let state = response.getState();
@@ -30,7 +29,6 @@
                 //Set up the categories
                 let categories = response.getReturnValue();
                 component.set("v.categories", categories);
-                console.log("catefories from apex: " + JSON.stringify(categories));
                 //Set the max number of categories
                 let catsize = categories.length - 1;
                 component.set("v.catsize", catsize);
@@ -49,13 +47,15 @@
     },
     
     appStateChange : function(component, event, helper) {
-        //Get the parameters from the event
-        let state = event.getParam("state");
+        //Get the event
+        let compEvent = $A.get("e.c:InterviewAppStateEvent");
+        //Set the parameters
+        /*compEvent.setParams({
+
+        });*/
+        //Fire the event
+        compEvent.fire();
         
-        //If state is at one make the component visable
-        if(state == 1){
-            
-        }
 
     },
     
@@ -74,8 +74,9 @@
                 button.set('v.disabled',true);
             }
             //Make sure the forward button is enabled
-            let button = component.find('forward');
-            button.set('v.disabled',false);
+            //let button = component.find('forward');
+            //button.set('v.disabled',false);
+            helper.buttonChange(component, whichOne);
         }
         else if(whichOne == "forward"){
             current += 1;
@@ -83,8 +84,9 @@
             //Get the size of the array
             let catsize = component.get("v.catsize");
             if(current == catsize){
-                let button = component.find('forward');
-                button.set('v.disabled',true);
+                //let button = component.find('forward');
+                //button.set('v.disabled',true);
+                helper.buttonChange(component, whichOne);
             }            
             //Make sure the previous button is enabled
             let button = component.find('backward');
--- force-app/main/default/aura/InterviewMain/InterviewMainHelper.js
+++ force-app/main/default/aura/InterviewMain/InterviewMainHelper.js
@@ -10,6 +10,49 @@
         });
         //Fire the event
         compEvent.fire();
-    }
+    },
     
+    buttonChange : function(component, whichOne){
+        let buttonId;
+        let buttonName;
+        let buttonIcon;
+        let buttonText;
+        let buttonAction;
+
+        //Figure out which button to change
+        if(whichOne == "forward"){
+            //Create the finish button parameters
+            buttonId = "finish";
+            buttonName = "finishButton";
+            buttonIcon = "utility:check";
+            buttonText = "Finish Interview",
+            buttonAction = "c.appStateChange";
+
+        }else if(whichOne == "backward"){
+            //Create the forward button parameters
+            buttonId = "forward";
+            buttonName = "forwardButton";
+            buttonIcon = "utility:chevronright";
+            buttonText = "Next Category";
+            buttonAction = "c.categoriesChange";
+        }
+        //Create the button
+        $A.createComponent(
+            "lightning:buttonIcon",
+            {
+                "aura:id": buttonId,
+                "name": buttonName,
+                "iconName": buttonIcon,
+                "alternativeText": buttonText,
+                "size" : "large",
+                "onclick": component.getReference(buttonAction)
+            },
+            function(newButton){
+                var divComponent = component.find("rightbutton");
+                divComponent.set("v.body",newButton);
+            }
+        );
+
+    }
+
 })

092084d7296757b10d1b13f7637a5545ff1ef6cf
yfedorovskyi@gmail.com <yfedorovskyi@gmail.com>
2019-06-21 10:48:55 -0400

    little cleanup

--- force-app/main/default/aura/InterviewMainProgress/InterviewMainProgressController.js
+++ force-app/main/default/aura/InterviewMainProgress/InterviewMainProgressController.js
@@ -1,33 +1,4 @@
 ({
-    doInit : function(component, event, helper) {
-        var allCats = component.get("v.allCategories");
-
-        // console.log("generating buttons with: " + JSON.stringify(allCats) + "length: " + allCats.length);
-        // helper.generateButtons(component, allCats);
-
-        // if (allCats.length != 0) {
-        // } else {
-        //     // Create the action
-        //     let action = component.get("c.getCategories");
-        //     // Add callback behavior for when response is received
-        //     action.setCallback(this, function(response) {
-        //         let state = response.getState();
-        //         if (state === "SUCCESS") {
-        //             // component.set("v.allCategories", response.getReturnValue());
-        //             let resp = response.getReturnValue();
-        //             helper.generateButtons(component, resp);
-        //             console.log("initiated with: \n" + JSON.stringify(resp));
-        //             // component.set("v.allCategories", resp);
-        //         }
-        //         else {
-        //             console.log("Failed with state: " + state);
-        //         }
-        //     });
-        //     // Send action off to be executed
-        //     $A.enqueueAction(action);
-        // }
-    },
-
     handleStatusChange : function(component, event, helper) {
         let allCats = component.get("v.allCategories");
         let currId = component.get("v.currentCat");
@@ -66,25 +37,5 @@
             helper.generateButtons(component, cats);
             component.set("v.allCategories", cats);
         }
-    },
-
-    tmpHandlePress : function(component, event, helper) {
-        // let statusChange = component.getEvent("categoryStatusChange");
-        // statusChange.setParams({
-        //     "status": 0
-        // });
-        // statusChange.fire();
-        // console.log("event categoryStatusChange fired!")
-
-        let categoriesChange = component.getEvent("categoriesChange");
-        let btnid = event.getSource().getLocalId();
-        categoriesChange.setParams({
-            "current": btnid[btnid.length-1]
-        });
-        categoriesChange.fire();
-        console.log("event categoriesChange fired!");
-
-        var allCats = component.get("v.allCategories");
-        console.log("all categories: " + JSON.stringify(allCats));
     }
 })

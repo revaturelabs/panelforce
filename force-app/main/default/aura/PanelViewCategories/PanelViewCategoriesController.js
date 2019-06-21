017f930265d19b8098a6a013b19b4baab8d8b9d1
Joshua Najera <switch620@gmail.com>
2019-06-21 11:22:40 -0400

    Cleaning up

--- force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesController.js
+++ force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesController.js
@@ -1,22 +1,22 @@
 // {!REQUIRESCRIPT('/soap/ajax/37.0/connection.js')}
 ({
 
+    // Fetches all the relevant records, stores them in v.records, and performs initial population.
     loadList: function(component, event, helper) {
         component.set("v.searchText", "");
         let assessmentID = component.get("v.recordId");
-        // console.log(assessmentID);
-        // result = sforce.connection.query("Select Name, Id FROM AssessmentLineItem__c WHERE PH_Assessment__c = " + assessmentID);
-        // records = result.getArray("AssessmentLineItem__c");
-        var action = component.get("c.fetchLineItems");
         var items;
+        // Performs the fetching using apex
+        var action = component.get("c.fetchLineItems");
         action.setParams({ PanelId: assessmentID });
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
+                // Save records returned from apex SOQL
                 items = response.getReturnValue();
                 component.set("v.records", items);
+                // Populate the list
                 items.forEach(listItem => {
-                    console.log("item: " + JSON.stringify(listItem));
                     helper.addListItem(component, listItem);
                 });
             }
@@ -24,39 +24,41 @@
         $A.enqueueAction(action);
     },
 
+    // Performs filtering acording to currently selected mode (All|Pass|Fail), and search text
     filterResults: function(component, event, helper) {
         let buttonSelection = component.get("v.buttonSelection");
         let searchText = component.get("v.searchText");
         let records = component.get("v.records");
+        // First clear v.body
         component.set("v.body", "");
         records.forEach(record => {
-            // Only add records with name containing searchText
+            // Don't add records without containing 'searchText' in the Name
             if (!record["Name"].toUpperCase().includes(searchText.toUpperCase())) {
                 return;
             }
-            // console.log("selection: " + buttonSelection);
-            // console.log("status: " + record["Status__c"]);
+            // Don't add failed records when filter mode is 'Pass'
             if (buttonSelection == "Pass" && record["Status__c"] == false) {
                 return;
             }
+            // Don't add passeded records when filter mode is 'Fail'
             if (buttonSelection == "Fail" && record["Status__c"] == true) {
                 return;
             }
+            // Add element if it passed through all previous filterings
             helper.addListItem(component, record);
-            // console.log("adding item");
-            // console.log("Name: " + record["Name"]);
         });
-        // console.log(searchText);
     },
 
+    // Set button selection, and appropriate styling when a button is clicked
     buttonClick: function(component, event, helper) {
         let source = event.getSource().getLocalId();
-        // console.log("Source: " + source);
         let buttonSelection = "";
         let buttonGroup = component.find("filterButtons").get("v.body");
+        // Reset buttons
         buttonGroup.forEach(button => {
             button.set("v.variant", "neutral");
         });
+        // Set corresponding button style and button filter mode
         switch (source) {
             case "allButton":
                 buttonSelection = "All";
@@ -74,47 +76,52 @@
                 console.log("error");
                 break;
         }
+        // Finalize button filter mode
         component.set("v.buttonSelection", buttonSelection);
     },
 
+    // Enable editing when clicking the field
     labelClick: function(component, event, helper) {
         console.log("label clicked!");
         event.getSource().set("v.readonly", false);
         // console.log(source);
     },
+
+    // Disable editing when field loses focus
     labelBlur: function(component, event, helper) {
-        console.log("label clicked!");
+        console.log("label blurred!" + event.getSource().getLocalId());
         event.getSource().set("v.readonly", true);
+        console.log(component.get("v.records"));
         // console.log(source);
     },
 
+    // Show comments when li element clicked
     liClick: function(component, event, helper) {
         console.log("Category list: " + component.find("CategoryList"));
         let componentBody = component.get("v.body");
         console.log("body: " + componentBody);
+        // First hide all comments
         for (let i = 1; i < componentBody.length; i++) {
             let localId = componentBody[i].getLocalId();
-            console.log("localId: " + localId);
             let currComment = component.find("Comment " + localId);
-            console.log("curr Comment: " + currComment);
             $A.util.addClass(currComment, "slds-hide");
         };
 
-        console.log("Event: " + event);
-
-
-        console.log("Comment " + event.srcElement["id"]);
         console.log("LI Clicked:" + component.find("Comment " + event.srcElement["id"]));
-        $A.util.removeClass(component.find("Comment " + event.srcElement["id"]), "slds-hide");
+        // console.log(component.find("Comment " + event.srcElement["id"]));
+        // Reveal selected element
+        let selectedComment = component.find("Comment " + event.srcElement["id"]);
+        $A.util.removeClass(selectedComment, "slds-hide");
     },
+
     liBlur: function(component, event, helper) {
         console.log("Things are getting blurry");
         $A.util.addClass(component.find("Comment" + event.srcElement["id"]), "slds-hide");
     },
 
+    // Fires the update event to the parent controller
     updateCategories: function(component, event, helper) {
         let customEvent = component.getEvent("updateCategoriesEvent");
-        // let records = [{...},{...}];
         let records = [{
             sobjectType: "Contact",
             Param1: "I_am_a_param",
--- force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesHelper.js
+++ force-app/main/default/aura/PanelViewCategories/PanelViewCategoriesHelper.js
@@ -6,25 +6,31 @@
         $A.createComponents(
             [
                 ["aura:html", { tag: "li", "aura:id": listItem["Name"], HTMLAttributes: { id: listItem["Name"], class: "slds-item CategoryItem", onclick: component.getReference("c.liClick"), onblur: component.getReference("c.liBlur") } }],
-                ["lightning:layout", { class: "CategoryLayout" }],
+                ["lightning:layout", { "aura:id": "id", class: "CategoryLayout", name: listItem["Id"] }],
+
                 // Category name
                 [
                     "lightning:layoutItem",
                     { class: "CategoryName", padding: "around-small" }
                 ],
                 ["lightning:input", { type: "text", name: "CategoryName", value: listItem["Name"], readonly: "true" }],
+
                 // Category Score
                 [
                     "lightning:layoutItem",
                     { class: "CategoryScore", flexibility: "no-grow", size: 3, padding: "around-small" }
                 ],
                 ["lightning:input", { type: "text", name: "Score", value: listItem["Score__c"], readonly: "true", onblur: component.getReference("c.labelBlur"), onclick: component.getReference("c.labelClick") }],
-                // Category pass/fail flag
+
+                // Icon container
                 [
                     "lightning:layoutItem",
                     { class: "CategoryResult", flexibility: "no-grow", size: 2, padding: "around-small" }
                 ],
+                // Comment
                 ["lightning:input", { type: "text", class: "slds-hide", "aura:id": "Comment " + listItem["Name"], name: "Comment", value: listItem["Comment__c"], readonly: "true", onblur: component.getReference("c.labelBlur"), onclick: component.getReference("c.labelClick") }],
+
+                // Icon
                 ["lightning:icon", { iconName: (listItem["Status__c"]) ? "action:approval" : "action:close", size: "xx-small" }]
             ],
             function(subparts, status, errorMessage) {
@@ -32,28 +38,33 @@
                 if (status === "SUCCESS") {
                     // Get component's v.body to populated list
                     var body = component.get("v.body");
+
                     // li and layout container
                     var liContainer = subparts[0];
                     var layout = subparts[1];
+
                     // Category Name
                     var categoryNameItem = subparts[2];
                     var categoryName = subparts[3];
                     categoryNameItem.set("v.body", categoryName);
+
                     // Category Score
                     var scoreItem = subparts[4];
                     var score = subparts[5];
                     scoreItem.set("v.body", score);
+
                     // Category flag icon
                     var iconItem = subparts[6];
                     var comment = subparts[7];
-                    console.log("Comment: " + comment);
-                    console.debug("listItem: " + listItem);
+
                     // console.log("Comment: " + listItem["Comment_c"]);
                     var icon = subparts[8];
                     iconItem.set("v.body", icon);
+
                     // Set, push, and commit
                     layout.set("v.body", [categoryNameItem, scoreItem, iconItem]);
                     liContainer.set("v.body", [layout, comment]);
+
                     // liContainer.get("v.body").push(comment);
                     body.push(liContainer);
                     component.set("v.body", body);

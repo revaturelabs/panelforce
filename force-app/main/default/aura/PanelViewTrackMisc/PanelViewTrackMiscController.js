233dc17fd74d2db62aada65d55488dc943458eed
CJRhodenbaugh <cjrhodenbaugh@outlook.com>
2019-06-22 15:10:40 -0400

    Updated Event

--- force-app/main/default/aura/PanelViewTrackMisc/PanelViewTrackMiscController.js
+++ force-app/main/default/aura/PanelViewTrackMisc/PanelViewTrackMiscController.js
@@ -1,42 +1,67 @@
 ({
     // Initialize component variables.
     init: function (cmp) {
-        cmp.set('v.editState', true);
-        cmp.set('v.readState', false);
-        cmp.set('v.comment', "Panel Comment");
+        cmp.set('v.comment', null);
 
     },
 
     // Pass button functionality.
     handlePass: function (cmp, event, helper) {
         alert("You clicked: " + event.getSource().get("v.label"));
+        let passClick = event.getSource();
+        let changeFail = cmp.get("v.fail");
+
+        passClick.iconName = "utility:success";
+        passClick.iconPosition = "right";
+
+        changeFail.removeAttribute("iconName");
+        changeFail.removeAttribute("iconPosition");
+
     },
 
     // Fail button functionality.
     handleFail: function (cmp, event, helper) {
         alert("You clicked: " + event.getSource().get("v.label"));
+        let failClick = event.getSource();
+        let changePass = cmp.get("v.pass");
+
+        failClick.iconName = "utility:clear";
+        failClick.iconPosition = "right";
+
+        changePass.removeAttribute("iconName");
+        changePass.removeAttribute("iconPosition");
+
     },
 
     // Componenet event that saves data from this component and sends it to the component with the save button.
     updateAssessment: function (cmp, event, helper) {
 
         // Get the component event by using the name value from aura:registerEvent.
-        var updateAssessmentEvent = $A.get("e.c:PanelViewTrackMiscEvent");
-
+        let updateAssessmentEvent = $A.get("e.c:PanelViewTrackMiscEvent");
+        
+        console.log("Begin Assessment");
         //References Custom Field names that may not exist. 
-        var assessment = {
-            sobjectType: "assessment__c",
+        let assessment = {
+            sobjectType: "PH_Assessment__c",
             Types_of_Associates: cmp.get("v.options"),
             Overall_Comment: cmp.get("v.comment"),
             Pass: cmp.get("v.pass"),
             Fail: cmp.get("v.fail"),
         };
-
+       
+        console.log("Update Assessment");
         //Updates update assessment field with variables and values from component.
         updateAssessmentEvent.setParams({
-            updateAssessment: assessment
+            "Assessment": assessment
         });
 
+        console.log("Begin Event Fire");
         updateAssessmentEvent.fire();
     },
+
+    // Handle Assessment Event
+    handleAssessment: function (cmp, event, helper) {
+
+        console.log("Event Fired");
+    }
 });

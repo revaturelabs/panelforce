b46e82ea169eb19844ca8293ea1047364dfba12b
Eli Pashrel <pashrel@gmail.com>
2019-06-21 20:11:55 -0400

    initial commit

--- force-app/main/default/aura/InterviewInitial/InterviewInitialController.js
+++ force-app/main/default/aura/InterviewInitial/InterviewInitialController.js
@@ -5,24 +5,12 @@
 
     cancel : function(component, event, helper) {
         $A.get("e.force:closeQuickAction").fire();
-        
-        //this.fireTheEvent();
-        //event.cancel;
-        //window.close();
-
-        ////let compEvent = $A.get();
-        //let compEvent = cmp.getEvent("c:interviewAppStateEvent");
-        //compEvent.setParams({});
-        //console.log(compEvent);
-        //compEvent.fire();
-        //console.log("event fired");
     },
 
     start : function(cmp, event) {
         // open interview window
         //window.open("InterviewApp");
         let compEvent = $A.get("e.c:InterviewAppStateEvent");
-        //let compEvent = cmp.getEvent("c:interviewAppStateEvent");
         compEvent.setParams({
             state : 1
         });
@@ -31,16 +19,9 @@
         console.log("event fired");
     },
 
-//    fireTheEvent : function(component, event) {
-//        var cmpEvent = component.getEvent("cmpEvent");
-//        cmpEvent.setParams({
-//            "message" : "The message: The component has fired."
-//        });
-//        cmpEvent.fire();
-//    },
-
     getTrackToo : function(component, event) {
-        console.log("in getTrack .");
+        // auto populate the Track Name field
+        // console.log("in getTrack .");
         var action = component.get("c.getTrack");
         action.setParams({"contactId" : component.get("v.recordId")});
         console.log(action);
@@ -56,6 +37,7 @@
     },
 
     getContact : function(component, event) {
+        // get the Contact record
         var action = component.get("c.getContact");
         action.setParams({"contact": component.get("v.recordId")});
         console.log(component.get("v.recordId") + ' ' + "c.getContact");
@@ -71,57 +53,4 @@
         });
         $A.enqueueAction(action);
     },
-
-//    doInit : function(component, event, helper) {        
-//        var action = component.get("c.getTrack");
-//        action.setParams({"contactId" : component.get("v.recordId")});
-//        console.log(action);
-//        action.setCallback(this, function(response) {
-//            var state = response.getState();
-//            if(state === "SUCCESS") {
-//                component.set("v.trackName", response.getReturnValue());
-//            } else {
-//                console.log('Problem getting track name, response state: ' + state);
-//            }
-//        });
-//        $A.enqueueAction(action);
-//        },
-//        
-//            handleSaveContact: function(component, event, helper) {
-//                if(helper.validateContactForm(component)) {
-//                    // Prepare the action to create the new contact
-//                    var saveContactAction = component.get("c.saveContactWithAccount");
-//                    saveContactAction.setParams({
-//                        "contact": component.get("v.newContact"),
-//                        "accountId": component.get("v.recordId")
-//                    });
-//        
-//                    saveContactAction.setCallback(this, function(response) {
-//                        var state = response.getState();
-//                        if(state === "SUCCESS") {
-//                            var resultsToast = $A.get("e.force:showToast");
-//                            resultsToast.setParams({
-//                                "title": "Contact Saved",
-//                                "message": "The new contact was created."
-//                            });
-//        
-//                            $A.get("e.force:closeQuickAction").fire();
-//                            resultsToast.fire();
-//                            $A.get("e.force:refreshView").fire();
-//                        }
-//                        else if (state === "ERROR") {
-//                            console.log('Problem saving contact, response state: ' + state);
-//                        }
-//                        else {
-//                            console.log('Unknown problem, response state: ' + state);
-//                        }
-//                    });
-//
-//                    $A.enqueueAction(saveContactAction);
-//                }
-//            },
-//        
-//            handleCancel: function(component, event, helper) {
-//                $A.get("e.force:closeQuickAction").fire();
-//            }
 })

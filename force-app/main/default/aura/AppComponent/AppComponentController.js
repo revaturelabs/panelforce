95f7b1f7da0815ebaa217aff6011e76e38886306
yfedorovskyi@gmail.com <yfedorovskyi@gmail.com>
2019-06-23 21:43:14 -0400

    fixed nuke

--- force-app/main/default/aura/AppComponent/AppComponentController.js
+++
@@ -1,7 +0,0 @@
-({
-    handleAppStateChange : function(component, event, helper) {
-        console.log('app state handled')
-        let newState = event.getParam("state");
-        component.set("v.appState", newState);
-    }
-})

d3708e1aba7f6da7044bb907a02329ce861b3c49
yfedorovskyi@gmail.com <yfedorovskyi@gmail.com>
2019-06-24 10:53:44 -0400

    generate  main app components in js 2

--- force-app/main/default/aura/AppComponent/AppComponentController.js
+++ force-app/main/default/aura/AppComponent/AppComponentController.js
@@ -1,7 +1,37 @@
 ({
+    doInit : function(component, event, helper) {
+        console.log("doInit on appComponent");
+        helper.generateElem(component, "c:InterviewInitial", {
+            "recordId" : component.get("v.recordId")
+        });
+        console.log("doInit on appComponent done");
+    },
+
     handleAppStateChange : function(component, event, helper) {
+        console.log("handleAppStateChange on appComponent");
+        let recordId = component.get("v.recordId");
+
         let newState = event.getParam("state");
+        let cats = event.getParam("categories");
+
         console.log('app state handled, new state: ' + newState);
         component.set("v.appState", newState);
+
+        if (newState == 1) {
+            helper.generateElem(component, "c:InterviewMain", {
+                "recordId" : recordId,
+                "categories" : cats
+            });
+            console.log("handleAppStateChange on appComponent newstate 1");
+
+        } else if (newState == 2) {
+            helper.generateElem(component, "c:PanelViewMain", {
+                "recordId" : recordId,
+                "categories" : cats
+            });
+            console.log("handleAppStateChange on appComponent newstate 2");
+        }
+        console.log("handleAppStateChange on appComponent done");
+        
     }
 })

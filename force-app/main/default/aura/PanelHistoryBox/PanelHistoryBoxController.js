813365b92551ebed2eee2f8d6032e137f241bc18
SebPannuto <pannuts@sunyit.edu>
2019-09-04 13:36:58 -0400

    Added PanelHistoryBox

---
+++ force-app/main/default/aura/PanelHistoryBox/PanelHistoryBoxController.js
@@ -0,0 +1,9 @@
+({
+    doInit : function(component, event, helper) {
+        // grabbing the attribute from the cmp
+        var IdComponent = "0033i000005ES39AAG"
+        // calling and setting params for the helper
+        let assessments = helper.getAssessmentHelper(component, IdComponent);
+     },
+            
+})

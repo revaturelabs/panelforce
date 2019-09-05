1b0bbcdc6f2bedac75bf34a5d5fb9d082f30a556
Baezh <baezh@kean.edu>
2019-09-05 13:16:50 -0400

    small changes

--- force-app/main/default/aura/AssessmentHistory/AssessmentHistoryController.js
+++ force-app/main/default/aura/AssessmentHistory/AssessmentHistoryController.js
@@ -2,7 +2,7 @@
     doInit : function(component, event, helper) {
         
         component.set('v.columns', [
-            {label: 'Attempts', fieldName: 'Attempts__c', type: 'Integer'},
+            {label: 'Attempt', fieldName: 'Attempts__c', type: 'Integer'},
             {label: 'Date', fieldName: 'Interview_Date__c', type: 'date'},
             {label: 'Panelist', fieldName: 'Panelist__c', type: 'text'},
             {label: 'Status', fieldName: 'OverallStatus__c', type: 'text'},

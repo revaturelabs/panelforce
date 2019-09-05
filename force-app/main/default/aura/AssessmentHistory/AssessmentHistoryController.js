8b3d6737c60c74940d3e841509c4b8e04f4b37ae
SebPannuto <pannuts@sunyit.edu>
2019-09-05 13:16:50 -0400

    added links on click

--- force-app/main/default/aura/AssessmentHistory/AssessmentHistoryController.js
+++ force-app/main/default/aura/AssessmentHistory/AssessmentHistoryController.js
@@ -9,7 +9,7 @@
         component.set("v.data", component.get("v.assessment"));
         
         component.set('v.columns', [
-            {label: 'Assessment Name', fieldName: 'Name', type: 'text'},
+            {label: 'Assessment Name', type: 'button', initialWidth: 100, typeAttributes: { label: {fieldName: 'Name'}, name: 'view_details', }},
             {label: 'Date', fieldName: 'Interview_Date__c', type: 'Date'},
             {label: 'Panelist', fieldName: 'Panelist__c', type: 'text'},
             {label: 'Status', fieldName: 'OverallPass__c', type: 'text'},
@@ -17,5 +17,13 @@
         ]);
                   
      },
+        handleRowAction : function (component, event, helper) {
+        var assessment = component.get("v.assessment");
+        var action = event.getParam('action');
+        if (action.name == 'view_details'){
+            var link = "https://panelforcesebp3-dev-ed.lightning.force.com/lightning/r/PH_Assessment__c/" + assessment.Id + "/view";
+            window.open(link,'_blank');
+        }
+    },
             
 })

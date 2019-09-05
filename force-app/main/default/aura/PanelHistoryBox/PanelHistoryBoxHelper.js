3440dadd96ee860b9a8e3be428a5ef99afed1842
Baezh <baezh@kean.edu>
2019-09-05 10:21:52 -0400

    added code to display actual pass or fail status

--- force-app/main/default/aura/PanelHistoryBox/PanelHistoryBoxHelper.js
+++ force-app/main/default/aura/PanelHistoryBox/PanelHistoryBoxHelper.js
@@ -4,12 +4,11 @@
         let listOfAssessments = []; 
         
         // Get a reference to server-side controller method
-        var getAssessments = component.get("c.getAssessments");
-        console.log("Assessments: " + getAssessments);   
+        var getAssessments = component.get("c.getAssessments");  
         
         // set parameters
         getAssessments.setParams({
-            "contactId" : "0033i000005ES39AAG" // component.get("v.recordId")
+            "contactId" : "0033i000004MKRSAA4" // component.get("v.recordId")
         }); // Pass Contact Id  as param 
         
         // Set the function to execute when the server gives a response (callback)
@@ -17,9 +16,9 @@
             var state = response.getState();
             
             if(state === "SUCCESS") {
-                listOfAssessments = response.getReturnValue();
                 console.log('Assessments from response: ' + response.getReturnValue());
                 console.log('Assessments from response: ' + JSON.stringify(response.getReturnValue()));
+                listOfAssessments = response.getReturnValue();
                 component.set("v.panels", listOfAssessments);
             } else if (state === "ERROR") {
                 console.log("Error");

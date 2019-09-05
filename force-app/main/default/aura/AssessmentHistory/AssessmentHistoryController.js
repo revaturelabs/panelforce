1487e981d5cffb16000e9d7be0c89d0443eb9051
Baezh <baezh@kean.edu>
2019-09-05 10:55:59 -0400

    changed assessment names to attempts

--- force-app/main/default/aura/AssessmentHistory/AssessmentHistoryController.js
+++ force-app/main/default/aura/AssessmentHistory/AssessmentHistoryController.js
@@ -2,7 +2,7 @@
     doInit : function(component, event, helper) {
         
         component.set('v.columns', [
-            {label: 'Assessment Name', fieldName: 'Name', type: 'text'},
+            {label: 'Attempts', fieldName: 'Attempts__c', type: 'Integer'},
             {label: 'Date', fieldName: 'Interview_Date__c', type: 'date'},
             {label: 'Panelist', fieldName: 'Panelist__c', type: 'text'},
             {label: 'Status', fieldName: 'OverallStatus__c', type: 'text'},

34eb8ec99c7717ad57ee13b8819ec634150a086b
futureSSE <nsddaniel@gmail.com>
2019-09-06 19:16:28 -0400

    Added functionality to save AssessmentLineItems

--- force-app/main/default/aura/PanelCategoryTest/PanelCategoryTestController.js
+++ force-app/main/default/aura/PanelCategoryTest/PanelCategoryTestController.js
@@ -1,15 +1,38 @@
 ({
     SaveALI : function(component, event, helper) {
         var action = component.get("c.saveALI");
+        
+        console.log('saveALI' + component.get("v.category"));
+        console.log('saveALI' + component.get("v.score"));
+        console.log('saveALI' + component.get("v.repanel"));
+        console.log('saveALI' + component.get("v.comments"));
         //action.setStorable();
         action.setParams({
-            "ALIId" : component.get("v.category"),
+            "ALIId" : component.get("v.category").Id,
             "score" : component.get("v.score"), 
             "repanel" : component.get("v.repanel"), 
             "comments" : component.get("v.comments")
         });
         action.setCallback(this, function(response) {
-            console.log("Progress Saved");
+            var state = response.getState();
+            
+            if (state === "SUCCESS") {
+                console.log('success');
+            }
+            else if (state === "INCOMPLETE") {
+                console.log('incomplete');
+            }
+                else if (state === "ERROR") {
+                    var errors = response.getError();
+                    if (errors) {
+                        if (errors[0] && errors[0].message) {
+                            console.log("Error message: " + 
+                                        errors[0].message);
+                        }
+                    } else {
+                        console.log("Unknown error");
+                    }
+                }
         });
         
         $A.enqueueAction(action);

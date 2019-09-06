eaf49cde7f74781461cb1e1ae17cadf4cfc2cda3
futureSSE <nsddaniel@gmail.com>
2019-09-06 15:33:31 -0400

    fixed syntax errors in AssessmentLineItemController

--- force-app/main/default/aura/PanelCategoryTest/PanelCategoryTestController.js
+++ force-app/main/default/aura/PanelCategoryTest/PanelCategoryTestController.js
@@ -1,13 +1,18 @@
 ({
     SaveALI : function(component, event, helper) {
-        var action = component.get("c.SaveALI");
-        action.setStorable();
-        action.setParams("{!v.category}","{!v.score}", "{!v.repanel}", "{!v.comments}");
-        action.setCallback(this, function(repsonse) {
+        var action = component.get("c.saveALI");
+        //action.setStorable();
+        action.setParams({
+            "ALIId" : component.get("v.category"),
+            "score" : component.get("v.score"), 
+            "repanel" : component.get("v.repanel"), 
+            "comments" : component.get("v.comments")
+        });
+        action.setCallback(this, function(response) {
             console.log("Progress Saved");
-        },
+        });
         
-          $A.enqueAction(action));
+        $A.enqueAction(action);
         
-    }
+    },
 })
